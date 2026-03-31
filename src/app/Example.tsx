5164e639c41b6db6fb200826fd0606739414a03d
rishikpulipaka <rpuli007@ucr.edu>
2026-03-31 13:34:32 -0700

    Example Componenet

---
+++ src/app/Example.tsx
@@ -0,0 +1,20 @@
+const Example = (props: { text1: string, text2: string, text3: string }) => {
+  return (
+    <div className="bg-bmes-blue-400 flex w-3/4 items-center gap-1 justify-self-center p-8">
+        <div className="bg-bmes-blue-200 w-1/2 p-5 text-left">
+        {props.text1}
+      </div>
+      <div className="flex w-1/2 flex-row gap-1">
+        <div className="bg-bmes-blue-200 w-full p-5 text-left">
+          {props.text2}
+        </div>
+        <div className="bg-bmes-blue-200 w-full p-5 text-right">
+          {props.text3}
+        </div>
+      </div>
+      {/* Hint: Add another box/div here similarly to line 4 */}
+    </div>
+  );
+};
+
+export default Example;
