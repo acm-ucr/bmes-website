364dcbc25b8a4455406184bd9a697f0f2d9fb793
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-12 14:02:54 -0700

    create array

---
+++ src/data/navigation.ts
@@ -0,0 +1,8 @@
+export const header_items = [
+    { name: 'Home', link: '/home'},
+    { name: 'About', link: '/about'},
+    { name: 'Board', link: '/board'},
+    { name: 'Mentorship', link: '/mentorship'},
+    { name: 'Gallery', link: '/gallery'},
+    { name: 'Contact', link: '/contact'},
+]
