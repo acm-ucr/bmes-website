fc0d3352e9154a51f6957feda8c696296116ed86
khurana-rhea <khuranarhea26@gmail.com>
2026-04-12 02:19:29 -0700

    Fixed Example.tsx

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,29 +1,19 @@
-const Example = (props: {
-  text1: string;
-  text2: string;
-  text3: string;
-  text4;
-}) => {
+const Example = (props: { text1: string; text2: string; text3: string; text4: string }) => {
   return (
     <div className="bg-bmes-blue-300 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
-      <div className="bg-bmes-blue-100 w-1/2 p-5 text-center">
-        rounded-full text-xl text-white {props.text1}
+      <div className="w-full rounded-full bg-bmes-blue-100 p-5 text-center text-xl text-white">
+        {props.text1}
       </div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-bmes-blue-100 w-full p-5 text-center">
-          text-xl
-          {props.text2}
-        </div>
-        <div className="bg-bmes-blue-100 w-full p-5 text-center">
-          text-xl
-          {props.text3}
-        </div>
+      <div className="w-full bg-bmes-blue-100 p-5 text-center text-xl">
+        {props.text2}
       </div>
-      <div className="bg-bmes-blue-300 w-full p-5 text-center">
-        rounded-full text-xl{props.text4}
+      <div className="w-full rounded-full bg-bmes-blue-100 p-5 text-center text-xl">
+        {props.text3}
+      </div>
+      <div className="w-full bg-bmes-blue-100 p-5 text-center text-xl">
+        {props.text4}
       </div>
     </div>
   );
 };
-
 export default Example;
