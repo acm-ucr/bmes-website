e1f4f694a1956b2252031c24ed8ac193e46b2f69
khurana-rhea <khuranarhea26@gmail.com>
2026-04-12 02:20:44 -0700

    Fix formatting

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,16 +1,21 @@
-const Example = (props: { text1: string; text2: string; text3: string; text4: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
     <div className="bg-bmes-blue-300 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
-      <div className="w-full rounded-full bg-bmes-blue-100 p-5 text-center text-xl text-white">
+      <div className="bg-bmes-blue-100 w-full rounded-full p-5 text-center text-xl text-white">
         {props.text1}
       </div>
-      <div className="w-full bg-bmes-blue-100 p-5 text-center text-xl">
+      <div className="bg-bmes-blue-100 w-full p-5 text-center text-xl">
         {props.text2}
       </div>
-      <div className="w-full rounded-full bg-bmes-blue-100 p-5 text-center text-xl">
+      <div className="bg-bmes-blue-100 w-full rounded-full p-5 text-center text-xl">
         {props.text3}
       </div>
-      <div className="w-full bg-bmes-blue-100 p-5 text-center text-xl">
+      <div className="bg-bmes-blue-100 w-full p-5 text-center text-xl">
         {props.text4}
       </div>
     </div>
