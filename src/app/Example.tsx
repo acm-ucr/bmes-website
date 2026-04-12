8b2b4b6af8c7a60144694f705b3836c7f919965f
khurana-rhea <khuranarhea26@gmail.com>
2026-04-12 02:51:01 -0700

    Fixed the colors

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -5,19 +5,19 @@ const Example = (props: {
   text4: string;
 }) => {
   return (
-    <div className="bg-bmes-blue-300 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
-      <div className="bg-bmes-blue-100 w-full p-5 text-center text-xl text-white">
+    <div className="bg-bmes-blue-100 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
+      <div className="bg-bmes-blue-300 w-full p-5 text-center text-xl text-white">
         {props.text1}
       </div>
       <div className="flex w-full flex-row gap-4">
-        <div className="bg-bmes-blue-100 w-full rounded-full p-5 text-center text-xl">
+        <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center text-xl">
           {props.text2}
         </div>
-        <div className="bg-bmes-blue-100 w-full rounded-full p-5 text-center text-xl">
+        <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center text-xl">
           {props.text3}
         </div>
       </div>
-      <div className="bg-bmes-blue-100 w-full p-5 text-center text-xl">
+      <div className="bg-bmes-blue-200 w-full p-5 text-center text-xl">
         {props.text4}
       </div>
     </div>
