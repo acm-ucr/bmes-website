2535c7cae069053f723473fbcf3b8999bd518b42
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-12 14:21:33 -0700

    imported navbar to home page for testing purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Example from "./Example";
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
+    <>
+    
+
+    <div>
+      <Navbar />
+    </div>
     <div className="flex h-screen w-screen items-center justify-center">
       <Example text1="Adjust" text2="These" text3="Props" />
     </div>
+    </>
   );
 };
 
