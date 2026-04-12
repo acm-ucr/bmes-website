63f726f37388969ed6bb87cca45b3b8434f1a28e
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-12 15:17:25 -0700

    formatted correctly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,33 +5,38 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <>
-        <div className="flex items-center justify-between p-5 bg-bmes-blue-100 text-white">
-            <Link href={header_items[0].link} className="flex flex-row items-center gap-2">
-                <Image src="/BMES_logo.png" alt="BMES Logo" width={40} height={40} />
-                <div className="flex flex-col">
-                    <span>BIOMEDICAL</span>
-                    <span>ENGINEERING</span>
-                    <span>SOCIETY</span>
-                </div>
-            </Link>
-            <Link href={header_items[1].link}>
-                {header_items[1].name}
-            </Link>
-            <Link href={header_items[2].link}>
-                {header_items[2].name}
-            </Link>
-            <Link href={header_items[3].link}>
-                {header_items[3].name}
-            </Link>
-            <Link href={header_items[4].link}>
-                {header_items[4].name}
-            </Link>
-            <Link href={header_items[5].link}>
-                {header_items[5].name}
-            </Link>
-            <Link href={header_items[6].link} className="bg-bmes-blue-300 px-4 py-2 rounded-2xl">
-                {header_items[6].name}
-            </Link>
+        <div className="flex items-center justify-between px-6 py-4 bg-bmes-blue-100 text-white">
+            <div className="pl-12">
+                <Link href={header_items[0].link} className="flex flex-row items-center gap-2">
+                    <Image src="/BMES_logo.png" alt="BMES Logo" width={40} height={40} />
+                    <div className="flex flex-col">
+                        <span>BIOMEDICAL</span>
+                        <span>ENGINEERING</span>
+                        <span>SOCIETY</span>
+                    </div>
+                </Link>
+            </div>
+
+            <div className="flex items-center gap-8 pr-10 text-lg">
+                <Link href={header_items[1].link}>
+                    {header_items[1].name}
+                </Link>
+                <Link href={header_items[2].link}>
+                    {header_items[2].name}
+                </Link>
+                <Link href={header_items[3].link}>
+                    {header_items[3].name}
+                </Link>
+                <Link href={header_items[4].link}>
+                    {header_items[4].name}
+                </Link>
+                <Link href={header_items[5].link}>
+                    {header_items[5].name}
+                </Link>
+                <Link href={header_items[6].link} className="bg-bmes-blue-300 px-4 py-2 rounded-2xl">
+                    {header_items[6].name}
+                </Link>
+            </div>
         </div>
         </>
     );
