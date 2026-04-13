516bf85ce6ffb310a9f71c307525026f7030ad95
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-13 16:50:00 -0700

    run format

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,8 @@ const Navbar = () => {
     <>
       <div className="bg-bmes-blue-100 flex items-center justify-between px-6 py-4 text-white">
         <div>
-          <Link
-            href="/"
-            className="flex items-center gap-2"
-          >
-            <Image
-              src={BMES_logo}
-              alt="BMES Logo"
-            />
+          <Link href="/" className="flex items-center gap-2">
+            <Image src={BMES_logo} alt="BMES Logo" />
             <div className="flex flex-col">
               <span>BIOMEDICAL</span>
               <span>ENGINEERING</span>
@@ -25,14 +19,17 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center gap-8 pr-10 text-lg">
-            {navigations.slice(1, 6).map(({ link, name}, index) => (
-                <Link key={index} href={link}>
-                    {name}
-                </Link>
-            ))}
-            <Link href="/contact" className="bg-bmes-blue-300 rounded-2xl px-4 py-2">
-                CONTACT US
+          {navigations.slice(1, 6).map(({ link, name }, index) => (
+            <Link key={index} href={link}>
+              {name}
             </Link>
+          ))}
+          <Link
+            href="/contact"
+            className="bg-bmes-blue-300 rounded-2xl px-4 py-2"
+          >
+            CONTACT US
+          </Link>
         </div>
 
         {/* <div className="flex items-center gap-8 pr-10 text-lg">
