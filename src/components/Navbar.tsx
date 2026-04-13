d7bd09663712f5c3b6118107745d55b8a85a25e5
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-13 16:22:13 -0700

    import image correctly and change info

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,20 @@
-import { header_items } from "@/data/navigation";
+import { navigations } from "@/data/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import BMES_logo from "@/public/BMES_logo.svg";
 
 const Navbar = () => {
   return (
     <>
       <div className="bg-bmes-blue-100 flex items-center justify-between px-6 py-4 text-white">
-        <div className="pl-12">
+        <div>
           <Link
-            href={header_items[0].link}
-            className="flex flex-row items-center gap-2"
+            href="/"
+            className="flex items-center gap-2"
           >
             <Image
-              src="/BMES_logo.png"
+              src={BMES_logo}
               alt="BMES Logo"
-              width={40}
-              height={40}
             />
             <div className="flex flex-col">
               <span>BIOMEDICAL</span>
@@ -26,16 +25,16 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center gap-8 pr-10 text-lg">
-          <Link href={header_items[1].link}>{header_items[1].name}</Link>
-          <Link href={header_items[2].link}>{header_items[2].name}</Link>
-          <Link href={header_items[3].link}>{header_items[3].name}</Link>
-          <Link href={header_items[4].link}>{header_items[4].name}</Link>
-          <Link href={header_items[5].link}>{header_items[5].name}</Link>
+          <Link href={navigations[1].link}>{navigations[1].name}</Link>
+          <Link href={navigations[2].link}>{navigations[2].name}</Link>
+          <Link href={navigations[3].link}>{navigations[3].name}</Link>
+          <Link href={navigations[4].link}>{navigations[4].name}</Link>
+          <Link href={navigations[5].link}>{navigations[5].name}</Link>
           <Link
-            href={header_items[6].link}
+            href={navigations[6].link}
             className="bg-bmes-blue-300 rounded-2xl px-4 py-2"
           >
-            {header_items[6].name}
+            {navigations[6].name}
           </Link>
         </div>
       </div>
