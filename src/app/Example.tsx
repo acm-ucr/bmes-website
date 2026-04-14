931475d727b42f76782957244c89beb1a4fa2d3d
Janko <jankojczanik@gmail.com>
2026-04-13 19:03:05 -0700

    test

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,16 +1,31 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="bg-bmes-blue-100 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-bmes-blue-300 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-bmes-blue-100 flex w-1/2 flex-col items-center justify-center gap-8 rounded-2xl p-8">
+      <div
+        className="w-1/2 p-5 text-center text-white"
+        style={{ backgroundColor: "#003067" }}
+      >
+        {props.text1}
+      </div>
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-bmes-blue-300 w-full p-5 text-left">
+        <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center">
           {props.text2}
         </div>
-        <div className="bg-bmes-blue-300 w-full p-5 text-right">
+        <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center">
           {props.text3}
         </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div
+        className="bg-bmes-blue-300 w-1/2 p-5 text-center"
+        style={{ backgroundColor: "#8fa2bd" }}
+      >
+        {props.text4}
+      </div>
     </div>
   );
 };
