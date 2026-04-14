03889575c1efe5d532b5a10bd54db80edc5d5c33
Kaesha Inocencio <freeforallyeet123@gmail.com>
2026-04-14 13:18:34 -0700

    fixed requested changes on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,37 @@
 "use client";
 import Link from "next/link";
 import { footerIcons } from "@/data/footer";
-import Logo from "@/public/bmes_website_logo.webp";
+import Logo from "@/public/bmes-logo.svg";
 import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="bg-bmes-blue-200 flex w-full py-2">
-      <div className="flex w-full items-center justify-between px-2">
-        <Image src={Logo} alt="BMES Logo" className="h-40 w-60 pt-20" />
+    <div className="bg-bmes-blue-200 flex w-full items-end justify-between py-3">
+      <div className="flex items-end gap-4">
+        <Image src={Logo} alt="BMES Logo" className="pl-5" />
+
+        <div className="text-bmes-gray-200 text-lg leading-tight">
+          <div>BIOMEDICAL</div>
+          <div>ENGINEERING</div>
+          <div>SOCIETY</div>
+        </div>
       </div>
-      <div aria-label="footer links" className="flex gap-4 pt-30 pr-5">
-        {footerIcons.map((icon) => {
-          const Icon = icon.icon;
-          return (
-            <Link
-              key={icon.name}
-              href={icon.href}
-              aria-label={icon.name}
-              target="_blank"
-              className="flex items-center gap-2"
-            >
-              <Icon className="text-bmes-gray-200 h-8 w-8"></Icon>
-            </Link>
-          );
-        })}
+
+      <div
+        aria-label="footer links"
+        className="flex items-center gap-4 pt-30 pr-5"
+      >
+        {footerIcons.map(({ href, name, icon: Icon }, index) => (
+          <Link
+            key={index}
+            href={href}
+            aria-label={name}
+            target="_blank"
+            className="text-bmes-gray-200 flex items-center justify-center text-3xl"
+          >
+            <Icon />
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,9 +1,16 @@
+import { IconType } from "react-icons";
 import { FaDiscord } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 
-export const footerIcons = [
+interface FooterLink {
+  name: string;
+  href: string;
+  icon: IconType;
+}
+
+export const footerIcons: FooterLink[] = [
   {
     name: "Discord",
     href: "https://discord.com/invite/7GNPMW7WSW?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn34U9YaLyDhICIaNzOHtLjvqxueBan3elDUjVTVFFzipfpvsyGMwyvgLvTUM_aem_si5PEemM00N8TnKpOVyGSA",
