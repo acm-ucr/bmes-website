5ae42bcf4ff128135c363e0f52ea9ec900cfb309
Janko <jankojczanik@gmail.com>
2026-04-15 11:42:31 -0700

    Changed to use an interface

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,30 +1,32 @@
-const Example = (props: {
+interface ExampleProps {
   text1: string;
   text2: string;
   text3: string;
   text4: string;
-}) => {
+}
+
+const Example = ({ text1, text2, text3, text4 }: ExampleProps) => {
   return (
     <div className="bg-bmes-blue-100 flex w-1/2 flex-col items-center justify-center gap-8 rounded-2xl p-8">
       <div
         className="w-1/2 p-5 text-center text-white"
         style={{ backgroundColor: "#003067" }}
       >
-        {props.text1}
+        {text1}
       </div>
       <div className="flex w-1/2 flex-row gap-1">
         <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center">
-          {props.text2}
+          {text2}
         </div>
         <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center">
-          {props.text3}
+          {text3}
         </div>
       </div>
       <div
         className="bg-bmes-blue-300 w-1/2 p-5 text-center"
         style={{ backgroundColor: "#8fa2bd" }}
       >
-        {props.text4}
+        {text4}
       </div>
     </div>
   );
