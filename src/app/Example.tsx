3ce6eb5a6fcb7b887b4c88e5147dc956addf257c
khurana-rhea <khuranarhea26@gmail.com>
2026-04-15 08:47:33 -0700

    Fixed the formatting using interface

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,9 +1,10 @@
-const Example = (props: {
+interface ExampleProps {
   text1: string;
   text2: string;
   text3: string;
   text4: string;
-}) => {
+}
+const Example = (props: ExampleProps) => {
   return (
     <div className="bg-bmes-blue-100 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
       <div className="bg-bmes-blue-200 w-full p-5 text-center text-xl text-white">
