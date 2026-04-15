df87fbfbf4edd8f162b939f248f0d1f753b9958b
khurana-rhea <khuranarhea26@gmail.com>
2026-04-15 13:20:07 -0700

    De-structured using interface

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -4,24 +4,25 @@ interface ExampleProps {
   text3: string;
   text4: string;
 }
-const Example = (props: ExampleProps) => {
+const Example = ({text1, text2, text3, text4}: ExampleProps) => {
   return (
     <div className="bg-bmes-blue-100 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded p-8">
       <div className="bg-bmes-blue-200 w-full p-5 text-center text-xl text-white">
-        {props.text1}
+        {text1}
       </div>
       <div className="flex w-full flex-row gap-4">
         <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center text-xl">
-          {props.text2}
+          {text2}
         </div>
         <div className="bg-bmes-blue-300 w-full rounded-full p-5 text-center text-xl">
-          {props.text3}
+          {text3}
         </div>
       </div>
       <div className="bg-bmes-gray-100 w-full p-5 text-center text-xl">
-        {props.text4}
+        {text4}
       </div>
     </div>
   );
 };
 export default Example;
+
