5f5d74654106d2b56a1b2c946266eee5f34f1b37
Himakiyay <esfahanimark@gmail.com>
2026-04-20 19:43:25 -0700

    made the set of second fixes

--- src/app/mentorship/page.tsx
+++ src/app/mentorship/page.tsx
@@ -1,18 +1,7 @@
 import AboutProgram from "@/components/mentorship/AboutProgram";
 
 const Page = () => {
-  return (
-    <AboutProgram
-      title="About The Program"
-      description="The Bioengineering Mentorship Program was established in conjunction with the"
-      linkText="Bioengineering Interdepartmental Graduate Student Association (BIG-GSA)"
-      linkHref="/"
-      subtext="Applications are currently open!"
-      buttonText="APPLY NOW"
-      buttonHref="/"
-      contactEmail="bmesmentorship@gmail.com"
-    />
-  );
+  return <AboutProgram />;
 };
 
 export default Page;
--- src/components/mentorship/AboutProgram.tsx
+++ src/components/mentorship/AboutProgram.tsx
@@ -5,54 +5,39 @@ import GearBottomRight from "@/public/gears/gear-bottom-right.svg";
 import GearTopRight from "@/public/gears/gear-top-right.svg";
 import GearLeft from "@/public/gears/gear-left.svg";
 
-interface AboutProgramProps {
-  title: string;
-  description: string;
-  linkText: string;
-  linkHref: string;
-  subtext: string;
-  buttonText: string;
-  buttonHref: string;
-  contactEmail: string;
-}
-
-const AboutProgram = ({
-  title,
-  description,
-  linkText,
-  linkHref,
-  subtext,
-  buttonText,
-  buttonHref,
-  contactEmail,
-}: AboutProgramProps) => {
+const AboutProgram = () => {
   return (
-    <section className="relative flex flex-col items-center overflow-hidden px-8 py-20 text-center">
+    <div className="relative flex flex-col items-center overflow-hidden px-8 py-20 text-center">
       <Image
         src={GearLeft}
         alt="Decorative gear on the left side"
-        className="pointer-events-none absolute -bottom-[80px] left-[0px] z-0 opacity-70"
+        className="pointer-events-none absolute -bottom-[80px] left-[0px] z-0"
       />
       <Image
         src={GearTopRight}
         alt="Decorative gear on the top right side"
-        className="pointer-events-none absolute -top-[20px] -right-[15px] z-0 opacity-70"
+        className="pointer-events-none absolute -top-[20px] -right-[15px] z-0"
       />
       <Image
         src={GearBottomRight}
         alt="Decorative gear on the bottom right side"
-        className="pointer-events-none absolute right-[0px] bottom-[10px] z-0 opacity-70"
+        className="pointer-events-none absolute right-[0px] bottom-[10px] z-0"
       />
 
-      <h1 className="text-bmes-blue-200 z-10 mb-6 text-4xl font-semibold">
-        {title}
-      </h1>
+      <div className="text-bmes-blue-200 z-10 mb-6 text-4xl font-semibold">
+        About The Program
+      </div>
 
       <p className="z-10 max-w-3xl text-lg leading-8 text-black">
-        {description}{" "}
-        <Link href={linkHref} className="text-bmes-blue-300 underline">
-          {linkText}
-        </Link>{" "}
+        The Bioengineering Mentorship Program was established in conjunction with the{" "}
+        <a
+          href="https://biggsa.engr.ucr.edu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-bmes-blue-300 underline"
+        >
+          Bioengineering Interdepartmental Graduate Student Association (BIG-GSA)
+        </a>{" "}
         to promote better interdepartmental relations in bioengineering.
         Undergraduate students are paired with a graduate or senior level
         student for the school year to help them achieve academic and
@@ -60,28 +45,30 @@ const AboutProgram = ({
         and how to apply for professional positions.
       </p>
 
-      <p className="z-10 mt-6 text-lg">{subtext}</p>
+      <p className="z-10 mt-6 text-lg">Applications are currently open!</p>
 
-      <Link
-        href={buttonHref}
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSe_JAmygafDcCn9ZOmhAtJMnvt12eJtTAhqDsywWScxiyF5Yg/viewform"
+        target="_blank"
+        rel="noopener noreferrer"
         className="bg-bmes-blue-300 z-10 mt-4 rounded-xl px-10 py-4 text-2xl font-bold text-white underline shadow-md transition hover:scale-105"
       >
-        {buttonText}
-      </Link>
+        APPLY NOW
+      </a>
 
       <p className="z-10 mt-8 text-lg">
         For more information, please contact{" "}
-        <Link
-          href={`mailto:${contactEmail}`}
+        <a
+          href="mailto:bmesmentorship@gmail.com"
           className="text-bmes-blue-300 underline"
         >
-          {contactEmail}
-        </Link>
+          bmesmentorship@gmail.com
+        </a>
         .
       </p>
 
       <div className="bg-bmes-blue-200 absolute bottom-0 left-0 h-2 w-full" />
-    </section>
+    </div>
   );
 };
 
