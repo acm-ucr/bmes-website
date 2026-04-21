cb3d553a4943c6ba5f8c84d924e82098fa97c65b
Kaesha Inocencio <freeforallyeet123@gmail.com>
2026-04-20 23:37:14 -0700

    did boardcard

---
+++ src/components/board/BoardCard.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { HiOutlineMail } from "react-icons/hi";
+
+interface CardInfo {
+  name: string;
+  image: StaticImageData;
+  position: string;
+  emailLink: string;
+}
+
+const BoardCard = ({ name, image, position, emailLink }: CardInfo) => {
+  return (
+    <div className="flex flex-col items-center text-center">
+      <div className="h-48 w-48 rounded-full bg-white shadow-xl">
+        <div className="relative h-full w-full overflow-hidden rounded-full">
+          <Image
+            src={image}
+            alt={name}
+            fill
+            className="scale-125 object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="mt-4 text-2xl text-black">{name}</div>
+
+      <div className="text-bmes-gray-100 mt-2 text-lg font-light uppercase">
+        {position}
+      </div>
+
+      <button
+        type="button"
+        className="border-bmes-blue-300 text-bmes-blue-300 mt-4 rounded-xl border-2 px-4 py-3 text-xl font-light uppercase"
+      >
+        About Me
+      </button>
+
+      <Link
+        href={emailLink}
+        aria-label={`Email ${name}`}
+        className="bg-bmes-blue-300 mt-6 flex h-20 w-20 items-center justify-center rounded-3xl shadow-md"
+      >
+        <HiOutlineMail className="h-15 w-15 text-white" />
+      </Link>
+    </div>
+  );
+};
+
+export default BoardCard;
