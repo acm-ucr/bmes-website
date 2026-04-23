8e323661620588d9cee3676bcde78f076cc2e172
SM Kamal <smkamal1005@gmail.com>
2026-04-22 21:43:40 -0700

    Navbar Responsified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { navigations } from "@/data/navigation";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import BMES_logo from "@/public/BMES_logo.svg";
-
+import { Menu, X } from "lucide-react";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
   return (
     <>
       <div className="bg-bmes-blue-100 flex items-center justify-between px-6 py-4 text-white">
@@ -18,7 +24,18 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="flex items-center gap-8 pr-10 text-lg">
+        <div
+          className="cursor-pointer lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <X className="h-12 w-12" />
+          ) : (
+            <Menu className="h-12 w-12" />
+          )}
+        </div>
+
+        <div className="hidden items-center gap-8 pr-10 text-lg lg:flex">
           {navigations.map(({ link, name }, index) => (
             <Link key={index} href={link}>
               {name}
@@ -32,6 +49,32 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
+
+      {isOpen && (
+        <div className="bg-bmes-blue-100 flex flex-col items-center gap-6 px-6 pb-6 text-2xl text-white lg:hidden">
+          {navigations.map(({ link, name }, index) => (
+            <Link
+              key={index}
+              href={link}
+              onClick={() => setIsOpen(false)}
+              className={`${
+                pathName === link
+                  ? "text-bmes-blue-200 flex h-16 w-screen items-center justify-center bg-white"
+                  : "no-underline"
+              }`}
+            >
+              {name}
+            </Link>
+          ))}
+          <Link
+            href="/contact"
+            onClick={() => setIsOpen(false)}
+            className="bg-bmes-blue-300 rounded-2xl px-4 py-2"
+          >
+            CONTACT US
+          </Link>
+        </div>
+      )}
     </>
   );
 };
