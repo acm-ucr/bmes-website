3a61170d1c6e7265080f101c8482b676360d4fcd
Janko <jankojczanik@gmail.com>
2026-04-26 09:48:53 -0700

    Fixed requested changes

--- src/components/about/Vision.tsx
+++ src/components/about/Vision.tsx
@@ -1,19 +1,16 @@
 const Vision = () => {
   return (
     <div
-      className="flex w-full flex-col items-center justify-center px-10 py-20"
-      style={{
-        background: "linear-gradient(180deg, #5699e6 0%, #003067 100%)",
-      }}
+className="flex w-full flex-col items-center justify-center px-10 h-[80vh] bg-linear-to-b from-bmes-blue-300 to-bmes-blue-200"
     >
-      <h2 className="mb-6 text-4xl text-white">Our Vision</h2>
-      <p className="mb-5 max-w-3xl text-center text-lg leading-8 text-white">
+      <div className="mb-6 text-5xl text-white">Our Vision</div>
+      <p className="mb-5 w-3/4 text-center text-2xl text-white">
         The vision of the Biomedical Engineering Society (BMES) is to serve as
         the world's leading society of professionals devoted to developing and
         using engineering and technology to advance human and well being.
       </p>
 
-      <p className="max-w-3xl text-center text-lg leading-8 text-white">
+      <p className="w-3/4 text-center text-2xl text-white">
         We at the UC Riverside chapter of this organization are dedicated in
         helping our members achieve their academic goals, whether they be
         pursuing graduate school, medical school, or a career in industry. The
