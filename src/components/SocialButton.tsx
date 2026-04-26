a5e9dc8fff66fcc463e08711f6d0eadadb929ae4
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-26 15:27:45 -0700

    props

---
+++ src/components/SocialButton.tsx
@@ -0,0 +1,13 @@
+import { StaticImageData } from "next/image";
+
+interface SocialButtonProps {
+    link: string;
+    icon: StaticImageData;
+}
+
+const SocialButton = ({ link, icon }: SocialButtonProps) => {
+    return (
+        <div></div>
+    );
+}
+export default SocialButton;
