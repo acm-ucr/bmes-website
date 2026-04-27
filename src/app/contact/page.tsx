88f556f68708ff159994cfe3ee2c05897a417b6d
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-26 17:09:37 -0700

    created bitton and link to discord

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,17 @@
+import SocialButton from "@/components/SocialButton";
+import { FaDiscord } from "react-icons/fa";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div>Contact</div>
+      <SocialButton 
+      link="https://discord.com/invite/7GNPMW7WSW?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn34U9YaLyDhICIaNzOHtLjvqxueBan3elDUjVTVFFzipfpvsyGMwyvgLvTUM_aem_si5PEemM00N8TnKpOVyGSA"
+      icon={FaDiscord}
+      />
+    </>
+
+  );
 };
 
 export default Contact;
--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -1,13 +1,16 @@
-import { StaticImageData } from "next/image";
+import Link from "next/link";
 
 interface SocialButtonProps {
     link: string;
-    icon: StaticImageData;
+    icon: React.ComponentType
 }
 
-const SocialButton = ({ link, icon }: SocialButtonProps) => {
+const SocialButton = ({ link, icon: Icon }: SocialButtonProps) => {
     return (
-        <div></div>
+        <Link href={link} target="_blank" className="flex items-center justify-center w-18 h-18 bg-bmes-blue-300 p-3 text-white text-5xl rounded-full shadow-lg">
+            <Icon />
+        </Link>
+
     );
 }
 export default SocialButton;
