a93357c10680066aca13c50b6869b05286732fea
paoloprogram12 <sethpsalazar@gmail.com>
2026-04-26 17:12:16 -0700

    run format

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,12 +5,11 @@ const Contact = () => {
   return (
     <>
       <div>Contact</div>
-      <SocialButton 
-      link="https://discord.com/invite/7GNPMW7WSW?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn34U9YaLyDhICIaNzOHtLjvqxueBan3elDUjVTVFFzipfpvsyGMwyvgLvTUM_aem_si5PEemM00N8TnKpOVyGSA"
-      icon={FaDiscord}
+      <SocialButton
+        link="https://discord.com/invite/7GNPMW7WSW?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn34U9YaLyDhICIaNzOHtLjvqxueBan3elDUjVTVFFzipfpvsyGMwyvgLvTUM_aem_si5PEemM00N8TnKpOVyGSA"
+        icon={FaDiscord}
       />
     </>
-
   );
 };
 
