6e5f4d702cad6e520ae2ae0264deccd7130bc6c1
Himakiyay <esfahanimark@gmail.com>
2026-04-27 18:47:08 -0700

    made changes

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import GearBottomRight from "@/public/gears/bottomRightGear.svg";
 import GearTopRight from "@/public/gears/topRightGear.svg";
@@ -10,44 +11,37 @@ const Landing = () => {
       <Image
         src={GearLeft}
         alt="Decorative gear on the left side"
-        width={300}
-        height={300}
         className="pointer-events-none absolute -bottom-[50px] left-[0px] z-0"
       />
       <Image
         src={GearTopRight}
         alt="Decorative gear on the top right side"
-        width={165}
-        height={165}
         className="pointer-events-none absolute -top-[0px] -right-[15px] z-0"
       />
       <Image
         src={GearBottomRight}
         alt="Decorative gear on the bottom right side"
-        width={225}
-        height={225}
         className="pointer-events-none absolute right-[0px] bottom-[0px] z-0"
       />
 
-      <p className="z-10 max-w-3xl text-lg leading-8 text-black">
-        Welcome to the University of California, Riverside chapter of BMES!{" "}
-        <br /> We aim to promote professional development and social skills
-        among
-        <br /> bioengineers at UCR through our events.
+      <p className="z-10 max-w-5xl text-3xl leading-8 text-black">
+        Welcome to the University of California, Riverside chapter of BMES! We
+        aim to promote professional development and social skills among
+        bioengineers at UCR through our events.
       </p>
 
       <div className="z-10 mt-12">
-        <p className="text-lg text-black">
+        <p className="text-3xl text-black">
           Check Us Out on <br /> Instagram BMES@UCR
         </p>
-        <a
+        <Link
           href="https://www.instagram.com/bmesatucr"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-bmes-gray-100 font-semibold underline"
+          className="text-bmes-gray-100 text-3xl font-semibold underline"
         >
           @bmesatucr
-        </a>
+        </Link>
       </div>
 
       <div className="bg-bmes-blue-200 absolute bottom-0 left-0 h-2 w-full" />
@@ -56,3 +50,5 @@ const Landing = () => {
 };
 
 export default Landing;
+
+//ask how pages are formatted, are they wrong? also about sizing of gears
