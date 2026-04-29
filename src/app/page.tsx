8b1a2682ed92eeab8a8ae418961b98e45107e462
khurana-rhea <khuranarhea26@gmail.com>
2026-04-29 12:42:53 -0700

    Made changes to the page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Mission from "@/components/about/Mission";
-import GetInTouch from "@/components/contact/GetInTouch";
 
 const Home = () => {
   return (
     <div>
       <Mission />
-      <GetInTouch />
     </div>
   );
 };
