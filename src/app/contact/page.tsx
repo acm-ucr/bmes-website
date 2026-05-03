bebd54ace7e09104889a693d1ebf9fa51b82f8b5
khurana-rhea <khuranarhea26@gmail.com>
2026-05-03 16:35:49 -0700

    Fix merge conflict

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import contactHeader from "@/public/headers/contactHeader.webp";
 import SocialButton from "@/components/SocialButton";
 import { FaDiscord } from "react-icons/fa";
+import GetInTouch from "@/components/contact/GetInTouch";
 
 const Contact = () => {
   return (
@@ -11,6 +12,7 @@ const Contact = () => {
         link="https://discord.com/invite/7GNPMW7WSW?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn34U9YaLyDhICIaNzOHtLjvqxueBan3elDUjVTVFFzipfpvsyGMwyvgLvTUM_aem_si5PEemM00N8TnKpOVyGSA"
         icon={FaDiscord}
       />
+      <GetInTouch />
     </div>
   );
 };
