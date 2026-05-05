d1da06434613314d1e73b33724a4cbda7b3af2f4
Himakiyay <esfahanimark@gmail.com>
2026-05-05 13:39:38 -0700

    made next round of fixes

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -1,37 +1,45 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import { HiOutlineMail } from "react-icons/hi";
+import AboutCard from "./AboutCard";
 
 interface CardInfo {
   name: string;
   image: StaticImageData;
   position: string;
   emailLink: string;
+  about: string;
 }
 
-const BoardCard = ({ name, image, position, emailLink }: CardInfo) => {
+const BoardCard = ({ name, image, position, emailLink, about }: CardInfo) => {
+  const [showAbout, setShowAbout] = useState(false);
   return (
-    <div className="flex flex-col items-center text-center">
+    <div className="relative flex flex-col items-center text-center">
       <Image src={image} alt={name} />
 
-      <div className="mt-4 flex h-8 items-center text-2xl text-black">
+      <div className="mt-4 flex h-16 items-center justify-center text-2xl text-black">
         {name}
       </div>
 
-      <div className="text-bmes-gray-100 mt-2 flex h-16 items-center justify-center text-lg font-light uppercase">
+      <div className="text-bmes-gray-100 mt-1 flex h-12 items-center justify-center text-lg font-light uppercase">
         {position}
       </div>
 
-      <div className="mt-2">
-        <button
-          type="button"
-          className="border-bmes-blue-300 text-bmes-blue-300 rounded-xl border-2 px-4 py-3 text-xl font-light uppercase"
-        >
-          About Me
-        </button>
-      </div>
+      <button
+        type="button"
+        onClick={() => setShowAbout(true)}
+        className="border-bmes-blue-300 text-bmes-blue-300 mt-4 cursor-pointer rounded-xl border-2 px-4 py-3 text-xl font-light uppercase"
+      >
+        About Me
+      </button>
+      {showAbout && (
+        <div className="fixed inset-50 z-50">
+          <AboutCard info={about} onClose={() => setShowAbout(false)} />
+        </div>
+      )}
 
       <Link
         href={emailLink}
--- src/components/board/BoardGrid.tsx
+++ src/components/board/BoardGrid.tsx
@@ -4,13 +4,14 @@ import boardInfo from "@/data/board";
 const BoardGrid = () => {
   return (
     <div className="mx-auto grid max-w-6xl grid-cols-1 justify-items-center gap-x-20 gap-y-16 px-6 py-16 sm:grid-cols-2 lg:grid-cols-4">
-      {boardInfo.map((member, index) => (
-        <div key={member.name} className={index === 16 ? "lg:col-start-2" : ""}>
+      {boardInfo.map(({ name, image, position, emailLink, about }, index) => (
+        <div key={name} className={index === 16 ? "lg:col-start-2" : ""}>
           <BoardCard
-            name={member.name}
-            image={member.image}
-            position={member.position}
-            emailLink={member.emailLink}
+            name={name}
+            image={image}
+            position={position}
+            emailLink={emailLink}
+            about={about}
           />
         </div>
       ))}
