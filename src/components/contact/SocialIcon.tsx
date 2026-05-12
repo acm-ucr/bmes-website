c2e4b844da2da3d5d0b0e881cf65b999b0c620e1
achin062@ucr.edu <achin062@ucr.edu>
2026-05-11 17:49:56 -0700

    fixing social media buttons

---
+++ src/components/contact/SocialIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+
+interface SocialIconProps {
+ icon: React.ComponentType;
+ url: string;
+}
+
+
+const SocialIcon = ({ icon, url }: SocialIconProps) => {
+ const Icon = icon;
+ return (
+   <Link
+     href={url}
+     target="_blank"
+     className="bg-bmes-blue-300 h-18 w-18 rounded-full p-3 text-5xl text-white shadow-lg"
+   >
+     <Icon />
+   </Link>
+ );
+};
+
+
+export default SocialIcon;
