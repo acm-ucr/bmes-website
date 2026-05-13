7cf8aa609f30ab72567e9ed04b60af8fce28f7b1
Himakiyay <esfahanimark@gmail.com>
2026-05-13 13:54:30 -0700

    changed the tags to div actually

--- src/components/contact/GetInTouch.tsx
+++ src/components/contact/GetInTouch.tsx
@@ -6,17 +6,17 @@ const GetInTouch = () => {
   return (
     <div className="mx-auto flex w-11/12 max-w-7xl flex-col items-center justify-center gap-8 rounded-2xl bg-white p-6 sm:p-8 md:p-10 lg:flex-row lg:gap-12">
       <div className="flex w-full flex-col gap-4 text-center lg:w-1/2 lg:text-left">
-        <h2 className="text-bmes-blue-100 text-3xl sm:text-4xl md:text-5xl">
+        <div className="text-bmes-blue-100 text-3xl sm:text-4xl md:text-5xl">
           Get in Touch!
-        </h2>
+        </div>
 
-        <p className="text-sm leading-relaxed sm:text-base md:text-lg">
+        <div className="text-sm leading-relaxed sm:text-base md:text-lg">
           Whether you are a first year interested in meeting new people or a
           third year looking into internships and learning opportunities, the
           Biomedical Engineering Society is the place for you!
-        </p>
+        </div>
 
-        <p className="text-sm leading-relaxed sm:text-base md:text-lg">
+        <div className="text-sm leading-relaxed sm:text-base md:text-lg">
           FOR MEMBERSHIP REQUESTS, please visit and join at{" "}
           <Link
             href="https://highlander.ucr.edu/organization/bmes"
@@ -25,7 +25,7 @@ const GetInTouch = () => {
             HighlanderLink
           </Link>{" "}
           and you will be added to our membership roster and mailing list!
-        </p>
+        </div>
       </div>
 
       <div className="flex w-full justify-center lg:w-1/2">
