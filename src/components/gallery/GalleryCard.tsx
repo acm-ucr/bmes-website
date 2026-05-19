31fb00db4982f94727b7d050c8a071d62b01d309
SM Kamal <smkamal1005@gmail.com>
2026-05-18 21:23:10 -0700

    comment fixes

--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import Image, { StaticImageData } from "next/image";
 
 interface ImageCardProps {
@@ -7,13 +10,19 @@ interface ImageCardProps {
 
 const ImageCard = ({ image, alt }: ImageCardProps) => {
   return (
-    <div className="relative h-full min-h-56 w-full overflow-hidden rounded-3xl">
+    <motion.div
+      initial={{ y: 10, opacity: 0.1, scale: 0.8 }}
+      whileInView={{ y: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: 0.7 }}
+      viewport={{ once: true }}
+      className="relative h-full min-h-56 w-full overflow-hidden rounded-3xl"
+    >
       <Image
         src={image}
         alt={alt}
         className="h-full w-full rounded-3xl object-cover"
       />
-    </div>
+    </motion.div>
   );
 };
 
