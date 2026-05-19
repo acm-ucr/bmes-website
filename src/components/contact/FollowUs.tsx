afb714af222b3c8a3cc627eb718399662b4f8742
achin062@ucr.edu <achin062@ucr.edu>
2026-05-19 11:54:50 -0700

    fix formatting

--- src/components/contact/FollowUs.tsx
+++ src/components/contact/FollowUs.tsx
@@ -4,11 +4,9 @@ import SocialIcon from "./SocialIcon";
 const FollowMap = () => {
   return (
     <div className="flex flex-col items-center gap-6 py-8">
-      <p className="text-bmes-blue-200 text-center text-5xl">
-        Follow us on
-      </p>
-      <div className="item-center mt-2 mb-10 flex justify-between w-1/2">
-        {SocialLinks.map(({icon, url}, index) => (
+      <p className="text-bmes-blue-200 text-center text-5xl">Follow us on</p>
+      <div className="item-center mt-2 mb-10 flex w-1/2 justify-between">
+        {SocialLinks.map(({ icon, url }, index) => (
           <SocialIcon key={index} icon={icon} href={url} />
         ))}
       </div>
