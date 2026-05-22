1e0b728265849c53f40c99e88bce658546e507d8
Kaesha Inocencio <kaesha@marshmallow.local>
2026-05-21 21:07:30 -0700

    responsified header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Header from "@/components/Header";
 import homeHeader from "@/public/headers/homeHeader.webp";
+import SmallHomeHeader from "@/public/headers/mobileHomeHeader.webp";
 import Landing from "@/components/home/Landing";
 
 const Home = () => {
@@ -8,6 +10,7 @@ const Home = () => {
       <Header
         title="BIOMEDICAL ENGINEERING SOCIETY AT UCR"
         image={homeHeader}
+        mobileImage={SmallHomeHeader}
       />
       <Landing />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,19 @@ import { usePathname } from "next/navigation";
 interface HeaderProps {
   title: string;
   image: StaticImageData;
+  mobileImage?: StaticImageData
   subtitle?: string;
 }
 
-const Header = ({ title, image, subtitle }: HeaderProps) => {
+const Header = ({ title, image, mobileImage, subtitle }: HeaderProps) => {
   const page = usePathname();
   return (
     <div className="pointer-events-none relative bg-white">
-      <Image src={image} alt={title} />
+      <Image src={image} alt={title} className="w-full hidden md:block"/>
+      <Image src={mobileImage || image} alt={title} className="w-full block md:hidden" />
+
       <div className="absolute inset-0 flex items-center justify-center text-center">
-        <div className="flex w-2/3 flex-col text-7xl font-light text-white">
+        <div className="flex w-2/3 flex-col text-7xl sm:text-10xl leading-tight font-light text-center text-white">
           {title}
           <div
             className={`mt-5 text-5xl font-light text-white ${page === "/mentorship" ? "italic underline" : ""}`}
