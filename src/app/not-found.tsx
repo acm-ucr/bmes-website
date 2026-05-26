737153bdfa6bfba68d131082343beb32a9c8f3ca
Himakiyay <esfahanimark@gmail.com>
2026-05-26 15:14:27 -0700

    made fixes to gears and labeled

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,48 @@
 import Link from "next/link";
-import gear404TopLeft from "@/public/gears/404TopGear.svg";
-import gear404BottomCenter from "@/public/gears/404BottomGear.svg";
-import gear404BottomRight from "@/public/gears/404RightGear.svg";
+import gear404TopLeftLarge from "@/public/gears/404TopGearLarge.svg";
+import gear404BottomCenterLarge from "@/public/gears/404BottomGearLarge.svg";
+import gear404BottomRightLarge from "@/public/gears/404RightGearLarge.svg";
+import gear404TopLeftSmall from "@/public/gears/404TopGearSmall.svg";
+import gear404BottomCenterSmall from "@/public/gears/404BottomGearSmall.svg";
+import gear404BottomRightSmall from "@/public/gears/404RightGearSmall.svg";
 import Image from "next/image";
 
 const NotFound = () => {
   return (
     <div className="relative flex min-h-[87vh] flex-col items-center justify-center overflow-hidden">
+
       <Image
-        src={gear404TopLeft}
-        alt=""
-        className="absolute top-5 left-0 -z-10"
+        src={gear404TopLeftSmall}
+        alt="Decorative gear"
+        className="absolute top-5 left-0 -z-10 md:hidden"
       />
       <Image
-        src={gear404BottomCenter}
-        alt=""
-        className="absolute bottom-0 left-2/3 -z-10 -translate-x-1/2 translate-y-1/2"
+        src={gear404TopLeftLarge}
+        alt="Decorative gear"
+        className="absolute top-0 left-0 -z-10 hidden md:block"
+      />
+
+
+      <Image
+        src={gear404BottomCenterSmall}
+        alt="Decorative gear"
+        className="absolute bottom-0 left-2/3 -z-10 -translate-x-1/2 translate-y-1/2 md:hidden"
       />
       <Image
-        src={gear404BottomRight}
-        alt=""
-        className="absolute right-0 bottom-5 -z-10"
+        src={gear404BottomCenterLarge}
+        alt="Decorative gear"
+        className="absolute bottom-0 left-1/4 -z-10 -translate-x-1/2 translate-y-1/2 hidden md:block"
+      />
+
+      <Image
+        src={gear404BottomRightSmall}
+        alt="Decorative gear"
+        className="absolute right-0 bottom-5 -z-10 md:hidden"
+      />
+      <Image
+        src={gear404BottomRightLarge}
+        alt="Decorative gear"
+        className="absolute right-0 bottom-0 -z-10 hidden md:block"
       />
 
       <div className="flex flex-col items-center text-center">
