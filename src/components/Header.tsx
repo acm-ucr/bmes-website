3bfa8b420ce96f1bebd0ea715a27f0154b1a29dc
Briana Garcia <briana_garcia_@outlook.com>
2026-05-27 14:36:54 -0700

    Add mobile headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
 import { usePathname } from "next/navigation";
+import defaultMobile from "@/public/mobileHeaders/mobileHeader.webp";
 
 interface HeaderProps {
   title: string;
@@ -12,19 +13,21 @@ interface HeaderProps {
 const Header = ({ title, image, mobileImage, subtitle }: HeaderProps) => {
   const page = usePathname();
   return (
-    <div className="pointer-events-none relative bg-white">
+    <div className="pointer-events-none relative">
       <Image src={image} alt={title} className="hidden w-full md:block" />
       <Image
-        src={mobileImage || image}
+        src={mobileImage || defaultMobile}
         alt={title}
         className="block w-full md:hidden"
       />
 
       <div className="absolute inset-0 flex items-center justify-center text-center">
-        <div className="lg:text-10xl flex flex-col place-self-center text-center text-4xl leading-tight font-light text-white sm:w-2/3 md:text-6xl lg:w-1/2">
+        <div
+          className={`lg:text-10xl flex flex-col place-self-center text-center leading-tight font-light text-white md:text-6xl ${page === "/" ? "text-4xl md:w-2/3" : "text-5xl"}`}
+        >
           {title}
           <div
-            className={`mt-5 text-5xl font-light text-white ${page === "/mentorship" ? "italic underline" : ""}`}
+            className={`mt-5 font-light text-white ${page === "/mentorship" ? "mt-2 text-3xl italic underline" : "text-5xl"}`}
           >
             {subtitle}
           </div>
