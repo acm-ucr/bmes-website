3b59c39b972d8bc837d193985d8e0d3a0ed65800
Briana Garcia <briana_garcia_@outlook.com>
2026-08-28 13:10:41 -0700

    apply small fixes to styling

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,16 @@
 import Header from "@/components/Header";
 import eventsHeader from "@/public/headers/eventsHeader.webp";
+import mobileEventsHeader from "@/public/mobileHeaders/mobileEvents.webp";
 import Upcoming from "@/components/events/Upcoming";
 
 const Events = () => {
   return (
     <div>
-      <Header title="Events" image={eventsHeader} />
+      <Header
+        title="Events"
+        image={eventsHeader}
+        mobileImage={mobileEventsHeader}
+      />
       <Upcoming />
     </div>
   );
